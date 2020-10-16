617ba7d924217e34e242ffab7c0645e27062dabb
Kohei-Ogino <kokyu9696@gmail.com>
2020-10-17 01:05:33 +0900

    Update alias file

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+
