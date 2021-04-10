7217157cf14f64e591cdffe65f91f5ede07c86d8
Kohei-Ogino <kokyu9696@gmail.com>
2021-04-11 02:09:06 +0900

    Add tmux

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,4 +3,4 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
